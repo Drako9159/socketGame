0e09448e0fa2fb3552cc62da28456133d920a6a2
drako9159 <drako9159@gmail.com>
2022-12-07 13:23:04 -0600

    add items selectors on app.js

---
+++ public/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userGrid = document.querySelector(".grid-user");
+  const cpuGrid = document.querySelector(".grid-cpu");
+  const displayGrid = document.querySelector(".grid-display");
+
+  const ships = document.querySelector(".ship");
+  const destroyer = document.querySelector(".destroyer-container")
+  const submarine = document.querySelector(".submarine-container")
+  const crusier = document.querySelector(".cusier-container")
+  const battleship = document.querySelector(".battleship-container")
+  const carrier = document.querySelector(".carrier-container")
+
+  const startButton = document.querySelector("#start")
+  const rotateButton = document.querySelector("#rotate")
+  const turnDisplay = document.querySelector("#turn")
+  const infoDisplay = document.querySelector("#info")
+
+  const setupButtons = document.querySelector("setup-buttons")
+});
