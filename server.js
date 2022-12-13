edef7953370805708aa012851a94f316935ce861
drako9159 <drako9159@gmail.com>
2022-12-12 18:43:12 -0600

    update socketgame

--- server.js
+++ server.js
@@ -1,61 +1,72 @@
-const express = require("express");
-const path = require("path");
-const http = require("http");
-
+const express = require('express');
+const path = require('path'); 
+const http = require('http');
 const PORT = process.env.PORT || 3000;
-const socket = require("socket.io");
+const socket = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 
-server.listen(PORT, () => console.log("El servidor está corriendo"));
-const connection = [null, null];
+server.listen(PORT, () => console.log("El servidor esta corriendo"));
 
-io.on("connection", (socket) => {
-  let playerIndex = -1;
-  for (let i in connection) {
-    if (connection[i] == null) {
-      playerIndex = i;
-      break;
+const connections = [null, null];
+
+io.on('connection', socket => {
+    let playerIndex = -1;
+    for (const i in connections) {
+        if (connections[i] === null) {
+            playerIndex = i;
+            break;
+        }
     }
-  }
-  socket.emit("player-number", playerIndex);
-  console.log(`Jugador ${player} se ha conectado`);
-  if (playerIndex === -1) return;
-  connection[playerIndex] = false;
 
-  socket.broadcast.emit("player-connection", playerIndex);
-  socket.on("disconnect", () => {
-    console.log(`Jugador ${player} se ha desconectado`);
-    connection[playerIndex] = null;
-    socket.broadcast.emit("player-connection", playerIndex);
-  });
-  socket.on("player-rady", () => {
-    socket.broadcast.emit("enemy-ready", playerIndex);
-    connection[playerIndex] = true
-  });
-  socket.io("check-players", () => {
-    const players = []
-    for( let i in connection){
-        connection[i] === null ? 
-        players.push({connected: false, ready: false}) :
-        players.push({connected: true, ready: connection[i]})
-    }
-    socket.emit("check-players", players);
-  })
-  socket.on("fire", id => {
-    console.log(`Disparo de ${playerIndex}`, id)
-    socket.broadcast.emit("fire", id);
-  })
-  socket.on("fire-reply", square => {
-    console.log(square)
-    socket.broadcast.emit("fire-reply", square);
-  })
-  setTimeout(() => {
-    connection[playerIndex] = null;
-    socket.emit("timeout")
-    socket.dissconnect()
-  }, 600000)
+    socket.emit("player-number", playerIndex);
+
+    console.log(`Jugador ${playerIndex} se ha conectado`);
+
+    if (playerIndex === -1) return;
+    connections[playerIndex] = false;
+
+    socket.broadcast.emit('player-connection', playerIndex);
+
+    socket.on('disconnect', () => {
+        console.log(`Jugador ${playerIndex} se ha desconectado`);
+        connections[playerIndex] = null;
+        socket.broadcast.emit('player-connection', playerIndex);
+    });
+
+    socket.on('player-ready', () => {
+        socket.broadcast.emit('enemy-ready', playerIndex);
+        connections[playerIndex] = true;
+    });
+
+    socket.on('check-players', () => {
+        const players = [];
+        for (const i in connections) {
+            connections[i] === null ? 
+                players.push({connected: false, ready: false}) : 
+                players.push({connected: true, ready: connections[i]});
+        }
+        socket.emit('check-players', players);
+    });
+
+    socket.on('fire', id => {
+        console.log(`Disparo de ${playerIndex}`, id);
+        socket.broadcast.emit('fire', id);
+    });
+
+    socket.on('fire-reply', square =>{
+        console.log(square);
+        socket.broadcast.emit('fire-reply', square);
+    });
+
+    setTimeout(() => {
+        connections[playerIndex] = null;
+        socket.emit('timeout');
+        socket.disconnect();
+    }, 600000);
+
 });
+
